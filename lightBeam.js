afd96d93483343d710bcc38a1e1300ee63c75fc8
Allie Soldau <alliesoldau@gmail.com>
2024-09-13 13:33:49 -0400

    added in some 45deg lights

--- lightBeam.js
+++ lightBeam.js
@@ -1,1 +1,7 @@
-// light beam
+window.addEventListener("load", function () {
+  const beamElements = document.querySelectorAll(".beam"); // Select all elements with the class 'beam'
+
+  beamElements.forEach(function (beam) {
+    beam.classList.add("start-animation"); // Add the animation class to each element
+  });
+});
